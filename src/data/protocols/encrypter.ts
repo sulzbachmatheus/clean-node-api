03ade1bdbb41c8f0cd9d116d30fd0188dc555d57
Matheus_Sulzbach <Matheus_Sulzbach@W10HM3LYY2>
2021-02-10 10:43:57 -0300

    ensure bcryptAdapter calls bcrypt with correct values

--- src/data/protocols/encrypter.ts
+++ src/data/protocols/encrypter.ts
@@ -1,3 +1,4 @@
 export interface Encrypter {
+  // async signature not allowed on interfaces, only allowed in the method implementations (concrete classes)
   encrypt: (value: string) => Promise<string>
 }
